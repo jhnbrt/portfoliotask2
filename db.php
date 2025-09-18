cbf79cd432b998b26207a903e1ec353c3e0c9cf5
jhnbrt <plameranjohnbert@gmail.com>
2025-09-18 16:15:03 +0800

    update

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";        // your DB username
-$pass = "";            // your DB password
-$db   = "portfolio_blog";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
 
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
